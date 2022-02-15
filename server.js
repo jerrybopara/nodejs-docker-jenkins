b7ecb32ba151e488d964f354f9aa4b43d884f5e5
Jerry Bopara <jerrybopara@gmail.com>
2022-02-15 16:53:33 +0530

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hi Jerry, I am working..') ;
+  res.send('Hi Jerry, I am working') ;
 });
 
 app.listen(PORT, HOST);
